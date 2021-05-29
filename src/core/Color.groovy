171519039342b04c6d7ed9c22e41b0dc56be2fdc
Didier Legrand <d.zebig@gmail.com>
2021-05-29 22:49:34 +0200

    sphere

--- src/core/Color.groovy
+++ src/core/Color.groovy
@@ -6,6 +6,16 @@ class Color extends Vec3 {
 		super(r, g, b)
 	}
 	
+	public Color(double r, double g, double b, double intensity) {
+		super(r * intensity, g * intensity, b * intensity)
+	}
+	
+	double r() { e[0] }
+	double g() { e[1] }
+	double b() { e[2] }
+	
+	Color plus(Color c) { new Color( e[0]+c.r(), e[1]+c.g(), e[2]+c.b()) }
+	
 	public String toString() {
 		int ir = (int)(255.999 * e[0])
 		int ig = (int)(255.999 * e[1])
--- src/main/CreateImage.groovy
+++ src/main/CreateImage.groovy
@@ -1,15 +1,27 @@
 package main
 
-import java.awt.Point
-
 import core.Color
+import core.Point3
 import core.Ray
 import core.Vec3
+
+
+boolean hit_sphere(final Point3 center, double radius, final Ray r) {
+    Vec3 oc = r.origin() - center
+    def a = Vec3.dot(r.direction(), r.direction())
+    def b = 2.0 * Vec3.dot(oc, r.direction());
+    def c = Vec3.dot(oc, oc) - radius*radius;
+    def discriminant = b*b - 4*a*c;
+    return (discriminant > 0);
+}
 
 Color ray_color(final Ray r) {
 	Vec3 unit_direction = r.direction().unit_vector()
 	def t = 0.5*( unit_direction.y() + 1.0 )
-	return (1.0 - t)*new Color(1,1,1) + t*new Color(0.5, 0.7, 1.0)
+	if (hit_sphere(new Point3(0,0,-1), 0.5, r)) {
+		return new Color(1,0,0, 2*t)
+	}
+	new Color(1,1,1, 1.0 - t) + new Color(0.5, 0.7, 1.0, t)
 }
 
 // Image
@@ -22,22 +34,27 @@ def viewport_height = 2.0
 def viewport_width = aspect_ratio * viewport_height
 def focal_length = 1.0
 
-def origin = new Point()
+def origin = new Point3(0,0,0)
+def horizontal = new Vec3(viewport_width, 0, 0)
+def vertical = new Vec3(0, viewport_height, 0)
+def lower_left_corner = origin - horizontal/2 - vertical/2 - new Vec3(0, 0, focal_length)
 
-def f = new File('sample.ppm')
 
+// Render
+
+def f = new File('sample3.ppm')
 def sb = new StringBuilder()
 
 for (int j = image_height-1; j >= 0; --j) {
 	System.err << "\rScanlines remaining: $j"
 	for (int i = 0; i < image_width; ++i) {
-		def r = (double)i / (image_width-1)
-		def g = (double)j / (image_height-1)
-		def b = 0.25
+		def u = (double)i / (image_width-1)
+		def v = (double)j / (image_height-1)
+		def r = new Ray( origin, lower_left_corner + horizontal*u + vertical*v - origin)
 		
-		def color = new Color(r,g,b)
+		def pixel_color = ray_color(r)
 		
-		sb << "$color\n"
+		sb << "$pixel_color\n"
 	}
 }
 
